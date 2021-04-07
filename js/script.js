5f029e1a334d8d32b620e82f5bbd942dc035ecb4
karin valcanover <valcanoverkarin@outlook.it>
2021-04-07 16:34:47 +0200

    genero 5 numeri casuali

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@
 //Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). 
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+//generare 5 numeri 
+var numeri = [];
+//totale numeri
+var numeriTotali = 5;
+// massimi numeri
+var numeriMassimi = 100;
+
+// Ciclo while finchè l'array ha 5 numeri diversi
+while(numeri.length < numeriTotali) {
+    var numeroGenerato = numeroCasuale(numeriMassimi);
+    if (numeri.includes(numeroGenerato) == false) {
+        numeri.push(numeroGenerato);
+    }
+}
+
+//funzione per generare un numero casuale
+function numeroCasuale(max) {
+    return Math.floor(Math.random() * max) + 1;
+}
+
+//i numeri generati
+console.log(numeri);
